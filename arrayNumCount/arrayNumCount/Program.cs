fef9b0c112b6e57c1262a8376501cb02fbba211e
Irfan Ahmed <34868734+irfanahmed89@users.noreply.github.com>
2022-12-31 04:49:51 -0500

    Add files via upload

---
+++ arrayNumCount/arrayNumCount/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace arrayNumCount
+{
+    class Program
+    {
+        const int SIZE = 12;
+        static void Main(string[] args)
+        {
+
+            int[] numbers = new int[SIZE] { 5, 5, 5, 7, 7, 7, 9, 7, 9, 9, 9, 1 };
+            int n = count(numbers, 5);
+            Console.WriteLine(n);
+        }
+
+        public static int count(int[] sampled, int val)
+        {
+
+        int count = 0;
+           for (int i = 0; i < sampled.Length; i++) {
+            if (sampled[i] == val) {
+             count++;
+           }
+        }
+          return count;
+      }
+    }
+}
